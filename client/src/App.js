212d30109acaad7c19927f20a1347ef15f5c7bc8
TaimurHasan <taimurhasan11@gmail.com>
2022-07-21 20:32:49 -0400

    minor changes to react imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Home from "./pages/Home.js"
