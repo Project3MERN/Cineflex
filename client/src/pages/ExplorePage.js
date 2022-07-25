fc51bea51e75100ab7c336461af5c161831eff4f
TaimurHasan <taimurhasan11@gmail.com>
2022-07-25 19:42:58 -0400

    returning postlist to explore page

--- client/src/pages/ExplorePage.js
+++ client/src/pages/ExplorePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Post from '../components/Post'
+import PostList from '../components/PostList'
 // import Comment from './Comment';
 
 function ExplorePage({ posts, setPosts }) {
@@ -8,11 +8,7 @@ function ExplorePage({ posts, setPosts }) {
         <div>
             <h2>Feed</h2>
             <div>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <PostList />
             </div>
         </div>
     )
