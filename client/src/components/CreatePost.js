982021ac357021542c19908ddacdcbb9240faec2
TaimurHasan <taimurhasan11@gmail.com>
2022-07-25 23:25:09 -0400

    adding update cache to add review logic, pending log error fixes

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -49,7 +49,8 @@ function CreatePost() {
                 query: GET_ALLREVIEWS,
                 data: { 
                     allReviews: {
-                        addReview, ...allReviews 
+                        ...allReviews,
+                        addReview
                     }
                 }
             });
