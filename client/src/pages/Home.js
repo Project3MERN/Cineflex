f633d12dfdd21f428f582cc63c5191980b3d1955
TaimurHasan <taimurhasan11@gmail.com>
2022-07-26 22:27:00 -0400

    implemented stripe donation option on homepage, pending success url

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from "react";
 import Post from "../components/Post";
 
-import { useQuery } from '@apollo/client';
-import { GET_ALLREVIEWS } from '../utils/queries';
+import { useQuery, useLazyQuery } from '@apollo/client';
+import { GET_ALLREVIEWS, QUERY_CHECKOUT } from '../utils/queries';
 import "../css/home.css";
 import { Link } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Home = () => {
+  
+  const [ donationAmount, setDonation ] = useState(0);
 
-  const { loading, data } = useQuery(GET_ALLREVIEWS)
-  const reviews = data?.allReviews || [];
-  // const [reviews, setReviews] = useState([])
+  const [getCheckout, { data: checkoutData, error }] = useLazyQuery(QUERY_CHECKOUT);
 
-  // useEffect(() => {
-  //   if(data) {
-  //     setReviews(data.allReviews)
-  //   }
-  // }, [data])
-  // console.log(reviews);
+  const handleDonationSubmit = async (e) => {
+    e.preventDefault();
+
+    const amount = parseInt(donationAmount);
+
+    getCheckout({
+      variables: { donation: amount }
+    });
+
+    console.log(error);
+  } 
+
+  useEffect(() => {
+    if(checkoutData) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: checkoutData.checkout.session })
+      })
+    }
+  })
 
   return (
     <div className="home">
@@ -35,6 +51,26 @@ const Home = () => {
         <button className="card_btn">
           <Link to="/signup">Sign Up!</Link>
         </button>
+      </div>
+      <div className="newsletter">
+        <h2>Donate to our growing team!</h2>
+        <form onSubmit = {handleDonationSubmit} className = "donationForm">
+          <label
+              htmlFor='donation'
+          >Enter Amount (USD):
+          </label>
+          <input
+              className="createPost-input"
+              type='number'
+              name='donation'
+              id='donation'
+              value={donationAmount}
+              onChange={(e) => setDonation(e.target.value)}
+          />
+            <button type ="submit" className="card_btn small_btn">
+              Donate!
+            </button>
+        </form>
       </div>
     </div>
   );
